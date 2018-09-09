62618ba391b3dbf4be54e2521753b24e234b966b
muyi-macpro <17621660286@163.com>
2018-09-09 23:17:01 +0800

    add route /blog and test show the blogs

--- www/handlers.py
+++ www/handlers.py
@@ -8,7 +8,8 @@ url handles.
 '''
 
 from webcore.coroweb import get, post
-from orm.models import User
+from orm.models import User, BLog
+import time
 
 @get('/')
 async def index(request):
@@ -18,3 +19,17 @@ async def index(request):
         'users': users
     }
 
+
+@get('/blogs')
+async def blogs(request):
+    summary = 'Lorem ipsum dolor sit amet, consectufsfm,sf sf sfsaffsf  wfweg f3fqw'
+    blogs = [
+        BLog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        BLog(id='2', name='Something new', summary=summary, created_at=time.time()-3600),
+        BLog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200),
+    ]
+    return { 
+        '__template__': 'blogs.html',
+        'blogs': blogs
+    }
+    
